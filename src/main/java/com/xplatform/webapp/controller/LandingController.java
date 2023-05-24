3e350d3da61290049235b48fa49dc6d6e11a5b6d
JordanWitt <wittjordan94@gmail.com>
2023-05-24 14:31:30 -0500

    landging page controller done

---
+++ src/main/java/com/xplatform/webapp/controller/LandingController.java
@@ -0,0 +1,25 @@
+package com.xplatform.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LandingController {
+    @PostMapping("/crossplatform")
+   public String handleCrossPlatform(){
+        return "/landing/landing";
+    }
+    @PostMapping("/login")
+    public String handleLoginBtn(){
+        return "/landing/login";
+    }
+    @PostMapping("/about")
+    public String handleAboutBtn(){
+        return "/landing/about";
+    }
+    @PostMapping("/contact")
+    public String handleContactBtn(){
+        return "/landing/contact";
+    }
+
+}
